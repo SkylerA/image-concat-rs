79dcf9161bb34250bf82c374f795b15074ade962
SkylerA <skylerarabak@gmail.com>
2024-05-03 15:38:08 -0700

    New Concat Fns Adding base concat function w/o loading that can do horiz or vert. Adding function to load and concat in columns.

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,17 @@ use std::cmp::max;
 use std::path::PathBuf;
 
 use image::io::Reader as ImageReader;
-use image::{ImageBuffer, ImageDecoder, Rgb};
+use image::{GenericImage, ImageBuffer, ImageDecoder, Rgb};
 
-// TODO add horizontal concat
+// TODO Make Image type more generic
+
+// NOTE: This currently assumes images are the same dimensions
 
 /// Loads given images and vertically concatenates them.
 /// Images are directly decoded into a single ImageBuffer to avoid unnecessary copying.
 ///
 /// # Arguments
-/// * `image_paths` - Vec of PathBufs to images to load
+/// * `image_paths` - Slice of PathBufs to images to load
 ///
 /// # Returns
 /// * `ImageBuffer<Rgb<u8>, Vec<u8>>`
@@ -20,11 +22,12 @@ use image::{ImageBuffer, ImageDecoder, Rgb};
 /// use image_concat_rs::load_and_vert_concat_images;
 /// use std::path::PathBuf;
 /// let img_result = load_and_vert_concat_images(&vec![PathBuf::from("./test/1.png"), PathBuf::from("./test/2.png")]);
+/// // or
+/// let img_result = load_and_vert_concat_images(&[PathBuf::from("./test/1.png"), PathBuf::from("./test/2.png")]);
 /// ```
 pub fn load_and_vert_concat_images(
-    image_paths: &Vec<PathBuf>,
+    image_paths: &[PathBuf],
 ) -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>, image::ImageError> {
-    // TODO See if threading this would improve speeds
     let mut total_height = 0;
     let mut max_width = 0;
 
@@ -70,4 +73,132 @@ pub fn load_and_vert_concat_images(
 
     // Return concatenated images
     Ok(buffer)
+}
+
+/// Loads given images and concatenate them into columns.
+/// Images are directly decoded into vertical columns to avoid unnecessary copying,
+/// but horizontal concatenation of those columns requires copying of already decoded images.
+///
+/// # Arguments
+/// * `image_paths` - Slice of PathBufs to images to load
+/// * `columns` - number of columns to split images into
+///
+/// # Returns
+/// * `ImageBuffer<Rgb<u8>, Vec<u8>>`
+///
+/// # Example
+/// ```
+/// use image_concat_rs::load_and_column_concat_images;
+/// use std::path::PathBuf;
+/// let img_result = load_and_column_concat_images(&[PathBuf::from("./test/1.png"), PathBuf::from("./test/2.png")], 2);
+/// ```
+pub fn load_and_column_concat_images(
+    image_paths: &[PathBuf],
+    columns: usize,
+) -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>, image::ImageError> {
+    // Vertical concatenation is more performant than horizontal because we can use the contiguous
+    // nature of the memory to directly decode images into a final buffer one after another without
+    // making copies of data. Horitontal concatenation would require decoding one row of each image
+    // into a final buffer before moving to the next line which I don't see a way to do in ImageDecoder.
+    // As such, we'll performantly vertical concatenate columns of images and then horizontally
+    // concatenate the columns into a single image buffer.
+    // Unfortunately, the horizontal concatenation will require explicitly copying memory over.
+
+    // vec to store our vertically concatenated columns
+    let mut col_buffs = Vec::new();
+
+    // Max number of images per column
+    let chunk_size = image_paths.len() / columns;
+    // Starting index of columns that will have less images
+    let chunk_remainder = image_paths.len() % columns;
+
+    // Build image columns
+    let mut start = 0;
+    for idx in 0..columns {
+        // Determine if this is a full size column or a partial column
+        let chunk_size = if idx < chunk_remainder {
+            chunk_size + 1
+        } else {
+            chunk_size
+        };
+        let end = start + chunk_size;
+
+        // Grab dynamic chunk size of images and concat verically
+        let buff = load_and_vert_concat_images(&image_paths[start..end])?;
+        col_buffs.push(buff);
+
+        start = end;
+    }
+
+    Ok(concat_images(&col_buffs, ConcatDirection::Horizontal)?)
+}
+
+pub enum ConcatDirection {
+    Vertical,
+    Horizontal,
+}
+/// Concatenates images vertically or horizontally
+///
+/// # Arguments
+/// * `images` - Slice of PathBufs to images to load
+/// * `direction` - ConcatDirection::Vertical or ConcatDirection::Horizontal
+///
+/// # Returns
+/// * `Result<ImageBuffer<Rgb<u8>, Vec<u8>, image::ImageError>`
+///
+/// # Example
+/// ```
+/// use image_concat_rs::{concat_images, ConcatDirection};
+/// let img1 = image::open("./test/1.png").unwrap().into_rgb8();
+/// let img2 = image::open("./test/2.png").unwrap().into_rgb8();
+/// let img_result = concat_images(&[img1,img2], ConcatDirection::Vertical);
+/// ```
+pub fn concat_images(
+    images: &[ImageBuffer<Rgb<u8>, Vec<u8>>],
+    direction: ConcatDirection,
+) -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>, image::ImageError> {
+    match direction {
+        ConcatDirection::Vertical => {
+            // Find the max width and total height of all images
+            let (max_width, total_height) =
+                images
+                    .iter()
+                    .fold((0, 0), |(max_width, total_height), img| {
+                        (max(max_width, img.width()), total_height + img.height())
+                    });
+
+            let mut buffer = ImageBuffer::new(max_width, total_height);
+
+            // Copy each image into the final buffer
+            let mut write_start = 0;
+            for img in images {
+                let write_end = write_start + img.height();
+                buffer.copy_from(img, 0, write_start)?;
+                write_start = write_end;
+            }
+
+            Ok(buffer)
+        }
+        ConcatDirection::Horizontal => {
+            // Find the total width and max height of all images
+            let (total_width, max_height) =
+                images
+                    .iter()
+                    .fold((0, 0), |(total_width, max_height), img| {
+                        (total_width + img.width(), max(max_height, img.height()))
+                    });
+
+            let mut buffer = ImageBuffer::new(total_width, max_height);
+
+            // Copy each image into the final buffer
+            let mut write_start = 0;
+            for img in images {
+                let write_end = write_start + img.width();
+                buffer.copy_from(img, write_start, 0)?;
+                write_start = write_end;
+            }
+
+            Ok(buffer)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::path::PathBuf;
 
-use image_concat_rs::load_and_vert_concat_images;
+use image::{ImageBuffer, Rgb};
+use image_concat_rs::{concat_images, load_and_column_concat_images, load_and_vert_concat_images};
+
+fn save_img(img: ImageBuffer<Rgb<u8>, Vec<u8>>, save_path: &str) {
+    match img.save_with_format(save_path, image::ImageFormat::Png) {
+        Ok(_) => println!("Saved image to {save_path}"),
+        Err(err) => println!("Error saving to {save_path}: {err}"),
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Make a Vec of image PathBufs
-    let img_count = 2;
+    let img_count = 8;
     let img_paths: Vec<_> = (1..=img_count)
         .map(|i| format!("./test/{}.png", i))
         .map(|s| PathBuf::from(&s))
@@ -12,13 +20,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Load and concat images
     let img = load_and_vert_concat_images(&img_paths)?;
+    save_img(img, "./load_and_vert_concat_images.png");
 
-    // Save image
-    let save_path = "./out.png";
-    match img.save_with_format(save_path, image::ImageFormat::Png) {
-        Ok(_) => println!("Saved image to {save_path}"),
-        Err(err) => println!("Error saving to {save_path}: {err}"),
-    }
+    let img = load_and_column_concat_images(&img_paths, 5)?;
+    save_img(img, "./load_and_column_concat_images.png");
+
+    let imgs: Vec<_> = img_paths
+        .iter()
+        .map(|path| image::open(path).unwrap().into_rgb8())
+        .collect();
+
+    let img = concat_images(&imgs, image_concat_rs::ConcatDirection::Vertical)?;
+    save_img(img, "./concat_images_vert.png");
+    let img = concat_images(&imgs, image_concat_rs::ConcatDirection::Horizontal)?;
+    save_img(img, "./concat_images_horiz.png");
 
     Ok(())
 }
